3f39ae126437e6477cb7fd7371483a3ff744d0bc
Dhruv Harish <dhruvharish26@gmail.com>
2026-09-16 00:10:08 +0530

    Finishing touch

--- src/components/dialog_share_subject.py
+++ src/components/dialog_share_subject.py
@@ -4,7 +4,7 @@ import io
 
 @st.dialog("Share class link")
 def share_subject_dialog(subject_name,subject_code):
-    app_domain="NoProxy-main.streamlit.app"
+    app_domain="noproxy-main.streamlit.app"
     join_url=f"{app_domain}/?join-code={subject_code}"
     
     
